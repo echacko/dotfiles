223a4a357a90f23d1aa792bca1cd5bc9130c0838
echacko <echacko@cewit.org.in>
2020-11-13 11:41:40 +0530

    Add tar.xz and adjust transparecy

--- Scripts/Scripts/extract.sh
+++ Scripts/Scripts/extract.sh
@@ -7,6 +7,7 @@ if [ -f $1 ] ; then
     case $1 in
         *.tar.bz2)   tar xjvf $1    ;;
         *.tar.gz)    tar xzvf $1    ;;
+        *.tar.xz)    tar xvJf $1    ;;
         *.bz2)       bunzip2 $1     ;;
         *.rar)       unrar e $1     ;;
         *.gz)        gunzip $1      ;;
