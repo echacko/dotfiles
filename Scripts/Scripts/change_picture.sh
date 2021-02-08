3d9ad7d0eaddc1bfb91e1d1bbe2fe9155d7bbe77
echacko <echacko@cewit.org.in>
2021-02-08 13:10:01 +0530

    Set single image as bg

--- Scripts/Scripts/change_picture.sh
+++ Scripts/Scripts/change_picture.sh
@@ -10,6 +10,10 @@ export DISPLAY=:0
 # Image directory
 PIC_DIR=/home/echacko/Pictures/Wallpapers
 
+# Now set the wallpaper
+image_file=$(ls -1 $PIC_DIR/ | sort -R | tail -1)
+feh --bg-fill --no-fehbg $PIC_DIR/$image_file
+
 # Link a random picutres for i3lock and lightdm
 image_file=$(ls -1 $PIC_DIR/ | sort -R | tail -1)
 
@@ -17,10 +21,7 @@ image_file=$(ls -1 $PIC_DIR/ | sort -R | tail -1)
 convert $PIC_DIR/$image_file $PIC_DIR/lockNlogin.png
 
 cp -f $PIC_DIR/lockNlogin.png /usr/share/backgrounds/.
-betterlockscreen -u $PIC_DIR
-
-# Now set the wallpaper
-feh --randomize --bg-fill --no-fehbg $PIC_DIR/*
+betterlockscreen -u $PIC_DIR 2&>1 &
 
 exit 0
 
