828bc4529aac9b54f978fc1c5c84acee67efefc4
echacko <echacko@cewit.org.in>
2021-03-29 15:56:49 +0530

    Misc changes

--- Scripts/Scripts/change_picture.sh
+++ Scripts/Scripts/change_picture.sh
@@ -12,7 +12,7 @@ PIC_DIR=/home/echacko/Pictures/Wallpapers
 
 # Now set the wallpaper
 image_file=$(ls -1 $PIC_DIR/ | sort -R | tail -1)
-feh --bg-fill --no-fehbg $PIC_DIR/$image_file
+feh --bg-fill $PIC_DIR/$image_file
 
 # Link a random picutres for i3lock and lightdm
 image_file=$(ls -1 $PIC_DIR/ | sort -R | tail -1)
@@ -21,7 +21,7 @@ image_file=$(ls -1 $PIC_DIR/ | sort -R | tail -1)
 convert $PIC_DIR/$image_file $PIC_DIR/lockNlogin.png
 
 cp -f $PIC_DIR/lockNlogin.png /usr/share/backgrounds/.
-betterlockscreen -u $PIC_DIR 2&>1 &
+betterlockscreen -u $PIC_DIR 2>&1 &
 
 exit 0
 
