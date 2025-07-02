3ce42da9e6e0943c6429615aaff8d7dc45eb44d3
echacko <echacko@cewit.org.in>
2025-07-02 22:18:32 +0200

    add treesitter plugin

---
+++ nvim/.config/nvim/lua/plguins/treesitter.lua
@@ -0,0 +1,84 @@
+return {
+  'nvim-treesitter/nvim-treesitter',
+  dependencies = {'nvim-treesitter/nvim-treesitter-textobjects'},
+  branch = 'master',
+  lazy = false,
+  build = ':TSUpdate',
+  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+  -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+  opts = {
+    ensure_installed = {
+      'bash',
+      'bibtex',
+      'c',
+      'cpp',
+      'diff',
+      'html',
+      'latex',
+      'lua',
+      'luadoc',
+      'markdown',
+      'markdown_inline',
+      'python',
+      'yaml'
+    },
+    -- Autoinstall languages that are not installed
+    auto_install = true,
+    highlight = { enable = true },
+    indent = { enable = true },
+    rainbow = {
+      enable = true,
+      extended_mode = true,
+    },
+    incremental_selection = {
+      enable = false,
+      keymaps = {
+        init_selection = "gnn",
+        node_incremental = "grn",
+        scope_incremental = "grc",
+        node_decremental = "grm",
+      },
+    },
+    indent = {
+      enable = true,
+    },
+    textobjects = {
+      -- https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+      select = {
+        enable = true,
+        keymaps = {
+          -- You can use the capture groups defined in textobjects.scm
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ac"] = "@class.outer",
+          ["ic"] = "@class.inner",
+        },
+      },
+      move = {
+        enable = true,
+        goto_next_start = {
+          ["]]"] = "@function.outer",
+          ["]m"] = "@class.outer",
+        },
+        goto_next_end = {
+          ["]["] = "@function.outer",
+          ["]M"] = "@class.outer",
+        },
+        goto_previous_start = {
+          ["[["] = "@function.outer",
+          ["[m"] = "@class.outer",
+        },
+        goto_previous_end = {
+          ["[]"] = "@function.outer",
+          ["[M"] = "@class.outer",
+        },
+      },
+      lsp_interop = {
+        enable = true,
+        peek_definition_code = {
+          ["Kf"] = "@function.outer",
+          ["KF"] = "@class.outer",
+        },
+      },
+    },
+  }
